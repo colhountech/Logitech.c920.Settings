cca942361ed5f3c6bb98e66de3a3d223f7355c6f
micheal <micheal@cts-ni.com>
2020-10-18 15:18:49 +0100

    Initial Commit

---
+++ Logitech.c920.Settings/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Logitech.c920.Settings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rtbStatus = new System.Windows.Forms.RichTextBox();
+            this.btnShowProperties = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 51);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(236, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(236, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load Video Sources";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rtbStatus
+            // 
+            this.rtbStatus.Font = new System.Drawing.Font("Trebuchet MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbStatus.Location = new System.Drawing.Point(12, 130);
+            this.rtbStatus.Name = "rtbStatus";
+            this.rtbStatus.Size = new System.Drawing.Size(1037, 308);
+            this.rtbStatus.TabIndex = 2;
+            this.rtbStatus.Text = "";
+            // 
+            // btnShowProperties
+            // 
+            this.btnShowProperties.Location = new System.Drawing.Point(12, 90);
+            this.btnShowProperties.Name = "btnShowProperties";
+            this.btnShowProperties.Size = new System.Drawing.Size(236, 23);
+            this.btnShowProperties.TabIndex = 3;
+            this.btnShowProperties.Text = "button2";
+            this.btnShowProperties.UseVisualStyleBackColor = true;
+            this.btnShowProperties.Click += new System.EventHandler(this.btnShowProperties_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1061, 450);
+            this.Controls.Add(this.btnShowProperties);
+            this.Controls.Add(this.rtbStatus);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rtbStatus;
+        private System.Windows.Forms.Button btnShowProperties;
+    }
+}
+
---
+++ Logitech.c920.Settings/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+using DirectShowLib;
+
+namespace Logitech.c920.Settings
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("oleaut32.dll", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        public static extern int OleCreatePropertyFrame( IntPtr hwndOwner, int x, int y, [MarshalAs(UnmanagedType.LPWStr)] string lpszCaption, int cObjects,[MarshalAs(UnmanagedType.Interface, ArraySubType=UnmanagedType.IUnknown)] ref object ppUnk, int cPages,IntPtr lpPageClsID, int lcid, int dwReserved, IntPtr lpvReserved);
+        //IMediaControl mediaControl = null;
+        //IGraphBuilder graphBuilder = null;
+        IBaseFilter theDevice = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (DsDevice ds in DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice))
+            {
+                comboBox1.Items.Add(ds.Name);
+            }
+
+            //Select first combobox item
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private IBaseFilter CreateFilter(Guid category, string friendlyname)
+        {
+            object source = null;
+            Guid iid = typeof(IBaseFilter).GUID;
+            foreach (DsDevice device in DsDevice.GetDevicesOfCat(category))
+            {
+                if (device.Name.CompareTo(friendlyname) == 0)
+                {
+                    device.Mon.BindToObject(null, null, ref iid, out source);
+                    break;
+                }
+            }
+
+            return (IBaseFilter)source;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {
+            //Release COM objects
+            if (theDevice != null)
+            {
+                Marshal.ReleaseComObject(theDevice);
+                theDevice = null;
+            }
+            //Create the filter for the selected video input device
+            string devicepath = comboBox1.SelectedItem.ToString();
+            rtbStatus.AppendText($"Selected {devicepath}");
+            rtbStatus.AppendText(Environment.NewLine);
+
+            theDevice = CreateFilter(FilterCategory.VideoInputDevice, devicepath);
+        }
+
+        private void btnShowProperties_Click(object sender, EventArgs e)
+        {
+            if (theDevice != null)
+            {
+                DisplayPropertyPage(theDevice);
+            }
+        }
+
+        private void DisplayPropertyPage(IBaseFilter theDevice)
+        {
+            ISpecifyPropertyPages pProp = theDevice as ISpecifyPropertyPages;
+            int hr = 0;
+
+            if (pProp == null)
+            {
+                return;
+            }
+
+            DsCAUUID caGUID;
+            hr = pProp.GetPages(out caGUID);
+            DsError.ThrowExceptionForHR(hr);
+
+            object oDevice = (object)theDevice;            
+            hr = OleCreatePropertyFrame(this.Handle, 0, 0, "", 1, ref oDevice, caGUID.cElems, caGUID.pElems, 0, 0, IntPtr.Zero);
+            DsError.ThrowExceptionForHR(hr);
+           
+            Marshal.FreeCoTaskMem(caGUID.pElems);
+            Marshal.ReleaseComObject(pProp);
+        }
+    }
+}
